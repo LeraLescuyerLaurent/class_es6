c90a7ebabf534ec8230b0b43b3bc3c45e91046ff
laurent lera <lllcommunication@gmx.fr>
2021-07-25 17:10:40 +0200

    utilisation des classes en js vanilla 1er test

---
+++ projet_es6_1/js/Titulaire.js
@@ -0,0 +1,7 @@
+export class Titulaire {
+    constructor(nom, prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+}
---
+++ projet_es6_1/js/script.js
@@ -0,0 +1,6 @@
+import { Titulaire } from "./Titulaire.js";
+
+let titulaire1 = new Titulaire();
+titulaire1.nom = "Dupont";  
+titulaire1.prenom = "Jean";
+console.log(titulaire1);
